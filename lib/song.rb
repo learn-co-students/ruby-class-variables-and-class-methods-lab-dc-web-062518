28767079673b85a833d24cd00e23b2b669a69e9b
Andres Manco <andresmanco@gmail.com>
2018-06-28 06:19:15 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << @genre
+    @@artists << @artist
+    @@count+=1
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genre_count
+     
+  end
+  
+  def self.artist_count
+    
+  end
+end
