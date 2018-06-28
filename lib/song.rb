7740b8e42d74b933556b0028d76dc3cc5d2de83c
github username <github email address>
2018-06-28 10:22:07 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -13,24 +13,32 @@ class Song
     @@artists << @artist
     @@count+=1
   end
-  
+
   def self.count
     @@count
   end
-  
+
   def self.genres
     @@genres.uniq
   end
-  
+
   def self.artists
     @@artists.uniq
   end
-  
+
   def self.genre_count
-     
+    hash = @@genres.group_by {|g| g}
+    hash.each do |k, v|
+      hash[k] = v.count
+    end
   end
-  
+
   def self.artist_count
-    
+    hash = @@artists.group_by {|g| g}
+    hash.each do |k, v|
+      hash[k] = v.count
+      
+    end
+
   end
 end
